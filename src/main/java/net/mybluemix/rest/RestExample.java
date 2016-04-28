010e367afaa1b07ba3a14c1d342c52a7dea1af0a
percival@gmail.com <plucena@gmail.com>
2016-04-28 13:22:15 -0300

    api

---
+++ src/main/java/net/mybluemix/rest/RestExample.java
@@ -0,0 +1,30 @@
+package net.mybluemix.rest;
+
+import javax.servlet.annotation.WebServlet;
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.PathParam;
+
+
+@Path("employee")
+public class RestExample {
+
+    @GET
+    public String employeeList(){
+    	return "All";
+    }
+    
+    //@Path("{name}")
+   /*
+    public String sayHello(@PathParam("name") String name){
+        StringBuilder stringBuilder = new StringBuilder("SandBox | Hello ");
+        stringBuilder.append(name).append("!");
+        return stringBuilder.toString();
+    }
+    */
+    
+    
+  
+
+
+}
