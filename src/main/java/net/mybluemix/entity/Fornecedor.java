6fd8bff8ea7e5fc28d7372b24e564af6aa545ab9
percival@gmail.com <plucena@gmail.com>
2016-05-09 17:24:41 -0300

    Add Fornecedor POST

--- src/main/java/net/mybluemix/entity/Fornecedor.java
+++ src/main/java/net/mybluemix/entity/Fornecedor.java
@@ -23,63 +23,53 @@ public class Fornecedor {
 	public String Endereco_Logradouro;
 	public String Endereco_CEP;
 	
-    @JsonProperty
+    
 	public String getCNPJ() {
 		return CNPJ;
 	}
 	
-    @JsonProperty
-	public void setCNPJ(String cNPJ) {
+    public void setCNPJ(String cNPJ) {
 		CNPJ = cNPJ;
 	}
 	
-    @JsonProperty
-	public String getNome() {
+    public String getNome() {
 		return nome;
 	}
 	
-    @JsonProperty
-	public void setNome(String nome) {
+    public void setNome(String nome) {
 		this.nome = nome;
 	}
 	
-    @JsonProperty
-	public String getEmail() {
+    public String getEmail() {
 		return email;
 	}
 	
-    @JsonProperty
-	public void setEmail(String email) {
+    public void setEmail(String email) {
 		this.email = email;
 	}
 	
-    @JsonProperty
-	public String getTelefone() {
+    public String getTelefone() {
 		return telefone;
 	}
 	
-    @JsonProperty
+    
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
 	
-    @JsonProperty
-	public int getPrazoEntregaDias() {
+    public int getPrazoEntregaDias() {
 		return PrazoEntregaDias;
 	}
 	
-    @JsonProperty
-	public void setPrazoEntregaDias(int prazoEntregaDias) {
+    public void setPrazoEntregaDias(int prazoEntregaDias) {
 		PrazoEntregaDias = prazoEntregaDias;
 	}
 	
-    @JsonProperty
-	public String getEndereco_Pais() {
+    public String getEndereco_Pais() {
 		return Endereco_Pais;
 	}
 	
-    @JsonProperty
-	public void setEndereco_Pais(String endereco_Pais) {
+    public void setEndereco_Pais(String endereco_Pais) {
 		Endereco_Pais = endereco_Pais;
 	}
 	
@@ -88,37 +78,31 @@ public class Fornecedor {
 		return Endereco_Estado;
 	}
 	
-    @JsonProperty
-	public void setEndereco_Estado(String endereco_Estado) {
+    public void setEndereco_Estado(String endereco_Estado) {
 		Endereco_Estado = endereco_Estado;
 	}
 	
-    @JsonProperty
-	public String getEndereco_Cidade() {
+    public String getEndereco_Cidade() {
 		return Endereco_Cidade;
 	}
 	
-    @JsonProperty
+    
 	public void setEndereco_Cidade(String endereco_Cidade) {
 		Endereco_Cidade = endereco_Cidade;
 	}
 	
-    @JsonProperty
 	public String getEndereco_Logradouro() {
 		return Endereco_Logradouro;
 	}
 	
-    @JsonProperty
 	public void setEndereco_Logradouro(String endereco_Logradouro) {
 		Endereco_Logradouro = endereco_Logradouro;
 	}
 	
-    @JsonProperty
 	public String getEndereco_CEP() {
 		return Endereco_CEP;
 	}
     
-    @JsonProperty
 	public void setEndereco_CEP(String endereco_CEP) {
 		Endereco_CEP = endereco_CEP;
 	}
