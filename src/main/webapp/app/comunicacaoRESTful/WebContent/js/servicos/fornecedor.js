47e62e272c4fe1fb215493b3607a6d0acb1c1ce8
Emanuel Felipe Duarte <efduarte@users.noreply.github.com>
2016-05-14 18:37:17 -0300

    Limpando o que não é usado

--- src/main/webapp/app/comunicacaoRESTful/WebContent/js/servicos/fornecedor.js
+++
@@ -1,9 +0,0 @@
-angular.module('ModuloFornecedor.services', []).factory('Fornecedor', function($resource) {
-	return $resource('http://loja.mybluemix.net/api/fornecedor/:cnpj', {
-		cnpj : '@cnpj'}, // Precisa ser igual a variável de onde veio.
-		{ 
-			update : {
-				method : 'PUT'
-			}
-		});
-});
