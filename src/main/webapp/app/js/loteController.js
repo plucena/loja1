a1337d04eb2809496ada269a74c3cb46131257bc
Emanuel Felipe Duarte <efduarte@users.noreply.github.com>
2016-05-24 16:59:20 -0300

    Front-end: ajustes

--- src/main/webapp/app/js/loteController.js
+++ src/main/webapp/app/js/loteController.js
@@ -1,8 +1,10 @@
+app.controller('loteController', ['$scope', '$rootScope', '$location', '$http', '$mdDialog', '$mdToast', 'FornecedorFactory', 'MateriaPrimaFactory', 'LoteFactory',  function($scope, $rootScope, $location, $http, $mdDialog, $mdToast, FornecedorFactory, MateriaPrimaFactory, LoteFactory) {
 
     
 
 }]);
 
+app.controller('loteController', ['$scope', '$routeParams', '$location', '$http', '$mdDialog', '$mdToast', '$document', 'FornecedorFactory', 'MateriaPrimaFactory', 'LoteFactory', function($scope, $routeParams, $location, $http, $mdDialog, $mdToast, $document, FornecedorFactory, MateriaPrimaFactory, LoteFactory) {
 
     
 
